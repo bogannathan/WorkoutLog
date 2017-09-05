3a4a9e2f69e618f285944a2742bcafd9947b0e5e
Nathan Bogan <bogannathan@gmail.com>
2017-09-05 13:32:33 -0400

    added models and db.js for refactor

--- workoutLogServer/app.js
+++ workoutLogServer/app.js
@@ -1,6 +1,12 @@
 var express=require('express')
 var app=express()
 var bodyParser = require('body-parser')
+var sequelize = require('./db')
+var User = sequelize.import('./models/user')
+
+User.sync() //User.sync({force:true})
+
+app.use(bodyParser.json())
 
 app.use(require('./middleware/headers'))
 
@@ -16,38 +22,6 @@ app.listen(3000, function() {
 //this file pulls in express and the headers.js file. it also gives response to the api/test url.
 //when server open it shows app open
 
-
-
-var Sequelize = require('sequelize')
-var sequelize = new Sequelize('workoutlog', 'postgres', '*Crumismyfuture17', {
-	host: 'localhost', 
-	dialect: 'postgres'
-})
-
-sequelize. authenticate().then(
-	function() {
-		console.log('connected to workoutlog postgres db')
-	},
-	function(err){
-		console.log(err)
-	}
-)
-
-var User = sequelize.define('user', {
-	username: Sequelize.STRING,
-	passwordhash: Sequelize.STRING,
-})
-
-
-//creates the table in postgres
-//matches the model we defined
-//doesn't drom the db
-
-User.sync()
-// DANGER THIS WILL DROP THE TABLEEEEEEEEEEEEEEEE
-//User.sync({force:true})
-//
-app.use(bodyParser.json())
 
 app.post('/api/user', function(req, res) {
 	// when we post to api user, it will want a user object in the body
@@ -69,5 +43,5 @@ app.post('/api/user', function(req, res) {
 		function createError(err){
 			res.send(500, err.message)
 		}
-)
+	)
 })
