7c4ccf9ac75cc46af6afe3e305117bf76838e43e
Nathan Bogan <bogannathan@gmail.com>
2017-09-05 09:24:09 -0400

    tested api

---
+++ workoutLogClient/app.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+	$("#testAPI").on("click", function(){
+		console.log("It's working?")
+	})
+	let test = $.ajax({
+		type: "GET",
+		url: "http://lcoalhost:3000/api/test"
+	})
+	.done(function(data){
+		console.log(data)
+	})
+	.fail(function(){
+		console.log("not work")
+	})
+})
