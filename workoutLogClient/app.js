6ff4db52bfcb8667c4c155a75e45c9c9ac3b51f1
Nathan Bogan <bogannathan@gmail.com>
2017-09-06 11:36:15 -0400

    added signup functionality

--- workoutLogClient/app.js
+++ workoutLogClient/app.js
@@ -1,17 +1,64 @@
 $(document).ready(function() {
-	$(testAPI).on("click", function(){
-		console.log("It's working?")
+	let WorkoutLog = (function($, undefined) {
+		let API_BASE = "http://localhost:3000/api/"
+		let userDefinitions= []
+
+		let setAuthHeader = function(sessionToken) {
+			window.localStorage.setItem("sessionToken", sessionToken)
+			//set the auth header
+			//this can be done on individual calls
+			//here we showcase ajaxsetup as a global tool
+			$.ajaxSetup({
+				"headers": {
+					"Authorization": sessionToken
+				}
+			})
+		}
+
+		// public
+		return {
+			API_BASE: API_BASE,
+			setAuthHeader: setAuthHeader
+		}
+	})(jQuery)
+
+	$(".nav-tabs a[data-toggle=tab]").on("click", function(e) {
+		let token = window.localStorage.getItem('sessionToken')
+		if ($(this).hasClass("disabled") && !token) {
+			e.preventDefault()
+			return false
+		}
 	})
-	let test = $.ajax({
-		type: "GET",
-		url: "http://localhost:3000/api/test"
+	//bind ab change events
+	$('a[data-toggle="tab"]').on('shown.bs.tab', function(e) {
+		let target = $(e.target).attr("href")
+		if (target === log) {
+			WorkoutLog.log.setDefinitions()
+		}
+
+		if (target === history) {
+			WorkoutLog.log.setHistory()
+		}
 	})
-	.done(function(data){
-		console.log(data)
+	//bind enter key
+	$(document).on('keypress', function(e) {
+		if (e.which === 13) {
+			if ($(signup-modal).is(':visible')) {
+				$(signup).trigger(click)
+			}
+			if ($(login-modal).is(':visible')) {
+				$(login).trigger('click')
+			}
+		}
 	})
-	.fail(function(){
-		console.log("not work")
-	})
+	//set header
+	let token = window.localStorage.getItem("sessionToken")
+	if (token) {
+		WorkoutLog.setAuthHeader(token)
+	}
+
+	//expose this to the other workoutlog modules 
+	window.WorkoutLog = WorkoutLog
 })
 
 // this file gets the info from api/test. it works because onready it runs let test and gets
---
+++ workoutLogClient/user/auth.js
@@ -0,0 +1,46 @@
+$(function(){
+	$.extend(WorkoutLog, {
+		//signup method
+		signup: function(){
+			//username and password variables
+			let username = $(su_username).val()
+			let password = $(su_password).val()
+		//user object
+			let user = {
+				user: {
+					username: username,
+					password: password
+				}
+			}
+			//signup post
+			let signup = $.ajax({
+				type: "POST",
+				url: WorkoutLog.API_BASE + "user",
+				data: JSON.stringify(user),
+				contentType:"application/json"
+			})
+			//signup done/fail
+			//.done() Promise
+            //Runs asynchronously
+			signup.done(function(data) {
+				if (data.sessionToken) {
+					WorkoutLog.setAuthHeader(data.sessionToken)
+					console.log("you made it")
+					console.log(data.sessionToken)
+				}
+
+				$(signupModal).modal("hide")
+				$('.disabled').removeClass("disabled")
+				$(loginout).text("Logout")
+			}).fail(function() {
+				$(su_error).text("There was an issue with sign up").show()
+			})
+		}
+
+		//login method
+
+		//loginout method
+	})
+	//bind events
+	$(signup).on('click', WorkoutLog.signup)
+})
