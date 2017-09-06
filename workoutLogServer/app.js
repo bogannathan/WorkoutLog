3d1a707613a1b51d0efd86a491f2ec781659c5bb
Nathan Bogan <bogannathan@gmail.com>
2017-09-06 09:25:56 -0400

    added session validation

--- workoutLogServer/app.js
+++ workoutLogServer/app.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 var express=require('express')
 var app=express()
 var bodyParser = require('body-parser')
@@ -10,11 +11,16 @@ User.sync() //User.sync({force:true})
 app.use(bodyParser.json()) // this needs to be in front of other app.use. it must jsonify 
 //so that other functions can use json objects
 app.use(require('./middleware/headers'))
+app.use(require('./middleware/validate-session'))
+//create a user
 app.use('/api/user', require('./routes/user'))
+//log in a user
+app.use('/api/login', require('./routes/session'))
+//localhose: 300/api/login/
+
 app.use('/api/test', function(req, res) {
 	res.send("Hello world")
 })
-app.use('/api/login', require('./routes/session'))
 
 app.listen(3000, function() {
 	console.log("app is open on 3000!")
